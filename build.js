a090d2f3d217692c860ffeb9c5a97c5e004a08db
Hassan Yousefi <hexboy2011@gmail.com>
2020-08-26 15:16:54 +0430

    update build package

--- build.js
+++ build.js
@@ -4,20 +4,18 @@ const version = require('./package.json')['version'];
 
 console.log({ version });
 
-if (!fs.existsSync(path.join(__dirname, `dist/${version}`))) {
-	fs.mkdirSync(path.join(__dirname, `dist/${version}`));
-}
-// move modules.js
+fs.renameSync(path.join(__dirname, 'dist'), path.join(__dirname, version));
+fs.mkdirSync(path.join(__dirname, 'dist'));
 fs.renameSync(
-	path.join(__dirname, 'dist/modules.js'),
-	path.join(__dirname, `dist/${version}/modules.js`)
+	path.join(__dirname, version),
+	path.join(__dirname, `dist/${version}`)
 );
 
 // move style.css
 fs.renameSync(
-	path.join(__dirname, 'dist/styles/main.css'),
+	path.join(__dirname, `dist/${version}/styles/main.css`),
 	path.join(__dirname, `dist/${version}/style.css`)
 );
 
 // remove style dir
-fs.rmdirSync(path.join(__dirname, 'dist/styles'));
+fs.rmdirSync(path.join(__dirname, `dist/${version}/styles`));
