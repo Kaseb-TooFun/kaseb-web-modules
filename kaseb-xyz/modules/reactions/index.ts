b3432086c13669ab31ef6f6d2b5390a5a6917b03
Hassan Yousefi <hexboy2011@gmail.com>
2020-09-03 01:15:17 +0430

    add urlPatterns option

--- kaseb-xyz/modules/reactions/index.ts
+++ kaseb-xyz/modules/reactions/index.ts
@@ -8,10 +8,22 @@ const applayReactions = (items: { id: string; value: string }[]) => {
 		const value =
 			typeof item.value == 'string' ? JSON.parse(item.value) : item.value;
 		const { data, type } = value;
+		const { urlPatterns } = data;
+		if (urlPatterns) {
+			let currentHref = document.location.href;
+			if (document.location.pathname == '/') {
+				currentHref = currentHref.replace(/\/$/g, '');
+			}
+			const isValid = urlPatterns.some((pattern) => {
+				return new RegExp(pattern).test(currentHref);
+			});
+			if (!isValid) return;
+		}
 		switch (type) {
 			case 'banner':
 			case 'modal':
 				return dispatchBanner(item.id, data);
+
 			case 'action':
 				return dispatchReaction(item.id, data);
 
@@ -61,10 +73,19 @@ export const initReactions = () => {
 			id: 'ed792a13-fd66-43b1-9359-0ec37d498856',
 			value:
 				'{"type":"banner","data":{"name":"my idle modal","template":"modal","title":"test title","description":"idle modal","bgColor":"#2e4057","textColor": "#fff","url":"https://hexboy.ir/","btnTextColor": "#2e4057","btnColor":"#fff","btnText":"press me!","condition":"idle-30","sourceSelector":".fa-twitter","isCloseable":true}}'
+		},
+		{
+			id: 'ed792a13-fd66-43b1-9359-0ec37d498857',
+			value:
+				'{"type":"banner","data":{"name":"on-hover banner","template":"bottom-banner", "fontFamily":"kio-Vazir","bgColor":"#2e4057","isRTL":true,"description":"تقاضا می‌گردد هر گونه ایراد یا کاستی در قلم می‌بینید و یا پیشنهادی برای بهتر شدن آن دارید در قسمت ایشوهای گیت‌هاب و یا از طریق آدرس saber.rastikerdar بر روی Gmail.com در میان بگذارید.","url":"https://hexboy.ir/","btnText":"press me!","condition":"wait-0","sourceSelector": ".avatar","isCloseable":true,"urlPatterns":["http://localhost:1234$", "/1235"]}}'
 		}
 	];
 	// return applayReactions(data);
-	if (!config.ignoreSavedReactions && config.id !== "demo" && config.id !== "preview") {
+	if (
+		!config.ignoreSavedReactions &&
+		config.id !== 'demo' &&
+		config.id !== 'preview'
+	) {
 		const baseUrl = process.env.BASE_URL || 'https://api.mykaseb.ir';
 		axios
 			.get(`${baseUrl}/api/v1/websites/${config.id}/configs`)
